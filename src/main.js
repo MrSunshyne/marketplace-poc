cbfe01fa515221e3e0a4e0c0e71bde8e51ae50d9
Sandeep Ramgolam <sandeep@ramgolam.com>
2020-06-08 18:54:51 +0400

    added tailwind configs added serif font added route and page removed some unused css

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import './assets/tailwind.css'
-
+import "./assets/tailwind.css";
+import "./assets/main.css";
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import ListingDetail from "../views/ListingDetail.vue";
 
 Vue.use(VueRouter);
 
@@ -8,14 +9,19 @@ const routes = [
   {
     path: "/",
     name: "home",
-    component: Home
-  }
+    component: Home,
+  },
+  {
+    path: "/listing/:id",
+    name: "detail",
+    component: ListingDetail,
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
